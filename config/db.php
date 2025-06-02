732f5c95d7fb3bafcb5309bf98469d6b1d51134f
WildanMukmin <wildanmukmin26@gmail.com>
2025-06-02 20:48:02 +0700

    setup project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+// Koneksi database
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'taskora';
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) die('Koneksi gagal: ' . mysqli_connect_error());
+?>
